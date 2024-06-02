6e5a241387325e80e48a2ebe07ed9a9f65eead7b
Alexey <sushak@mail.ru>
2024-06-02 08:51:37 +0300

    cors

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,13 +19,13 @@ class AuthController {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'None',
-        secure: false,
+        secure: true,
       });
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'None',
-        secure: false,
+        secure: true,
       });
       return res.json(userData);
     } catch (e) {
